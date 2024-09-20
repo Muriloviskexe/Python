19792cec67edf54c4e6a561b7445e20d551737cb
Murilo Garcia da Costa <murilogccontato@gmail.com>
2024-09-19 23:13:23 -0300

    novos labs

---
+++ Modulo2/LAB 2.6.9.py
@@ -0,0 +1,30 @@
+# Cenário
+# Sua tarefa é completar o código para avaliar os resultados de quatro operações aritméticas básicas.
+
+# Os resultados devem ser impressos no console.
+
+# Talvez você não consiga proteger o código de um usuário que deseja dividir por zero. Tudo bem, não se preocupe com isso por enquanto.
+
+# Teste seu código ‒ ele produz os resultados esperados?
+
+# Não mostraremos nenhum dado de teste - isso seria muito simples.
+
+
+a = int(input("insira um número aqui: "))
+b = int(input("Insita outro número aqui: "))
+
+
+# imprima o resultado da adição aqui
+print("a soma dos números informados é: ", a+b)
+
+# imprima o resultado da subtração aqui
+print("a subtração dos números informados é: ",a-b)
+
+# imprima o resultado da multiplicação aqui
+print("a multiplicação dos números informados é: ",a*b)
+
+# imprima o resultado da divisão aqui
+print("a divisão dos números informados é: ",a/b)
+
+
+print("\nIsso é tudo, pessoal!")
